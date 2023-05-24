f8e18a3627c266055ee9d6afd62f882fca760ccd
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-05-24 23:32:05 +0200

    Used linter

--- server/services/usersServices.ts
+++ server/services/usersServices.ts
@@ -5,8 +5,7 @@ const prisma = new PrismaClient()
 
 export const getAllUsers = async () => await prisma.user.findMany()
 export const createUser = async (userData: User) => await prisma.user.create({ data: userData })
-export const updateUser = async  (userData: User) => {
-
+export const updateUser = async (userData: User) => {
   await prisma.user.update({
     where: {
       id: userData.id
