e02bb1c00536f4065290c1c27bb7b7dc301e3154
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-05-24 23:31:36 +0200

    removed unused types

--- server/controllers/usersControllers.ts
+++ server/controllers/usersControllers.ts
@@ -1,7 +1,7 @@
 import * as usersServices from '../services/usersServices'
 import express from 'express'
 
-export const getAllUsers: any = async (_req: express.Request, res: express.Response) => {
+export const getAllUsers = async (_req: express.Request, res: express.Response) => {
   try {
     const users = await usersServices.getAllUsers()
     res.send(users)
@@ -10,14 +10,14 @@ export const getAllUsers: any = async (_req: express.Request, res: express.Respo
   }
 }
 
-export const createUser: any = async (req: express.Request, res: express.Response) => {
+export const createUser = async (req: express.Request, res: express.Response) => {
   const user = req.body
 
   const newUser = await usersServices.createUser(user)
   res.send(newUser)
 }
 
-export const updateUser: any = async (req: express.Request, res: express.Response) => {
+export const updateUser = async (req: express.Request, res: express.Response) => {
   const user = req.body
   const updatedUser = await usersServices.updateUser(user)
   res.send(updatedUser)
