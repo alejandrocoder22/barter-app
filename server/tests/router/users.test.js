94534c4e7af73320456535236e70d535944b8203
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-05-25 11:05:43 +0200

    Added mockupUser for testing

--- server/tests/router/users.test.js
+++ server/tests/router/users.test.js
@@ -1,12 +1,32 @@
 const request = require('supertest')
+import { after } from 'node:test'
+import * as usersServices from '../../services/usersServices'
 
 const app = require( '../../index.ts')
 
 describe('Tests In usersRouter', () => {
+
+  beforeAll( async () => {
+    const mockupUser = {
+      username: 'testUser',
+      password: '123456',
+      email: 'testUser@gmail.com'
+    }
+    await usersServices.createUser(mockupUser)
+  })
+
   test('GET /api/users Should return 200', async () => {
    const response = await request(app)
       .get('/api/users')
      
       expect(response.status).toBe(200)
+
+      console.log(response)
   })
+
+
+  afterAll(() => {
+    
+  })
+  
 })
