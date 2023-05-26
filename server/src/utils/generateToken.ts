d17182dbe797d5727ee679f9cc29e2fb2da51c82
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-05-26 22:35:01 +0200

    Created generateToken function

---
+++ server/src/utils/generateToken.ts
@@ -0,0 +1,18 @@
+import jwt from 'jsonwebtoken'
+import express from 'express'
+import { User } from '../types'
+
+
+const generateToken = (res: express.Response, user: User, secret: any) => {
+
+  const token = jwt.sign({userId: user.id, userName: user.userName}, secret)
+
+   res.cookie('jwt', token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV !== 'development',
+        sameSite: 'strict',
+        maxAge: 30 * 24 * 60 * 60 * 1000
+    })
+}
+
+export default generateToken
