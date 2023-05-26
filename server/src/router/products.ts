e68fc2b9669f71ee1c591be4442fd25c15009fb3
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-05-26 12:52:47 +0200

    Linter fixing

--- server/src/router/products.ts
+++ server/src/router/products.ts
@@ -2,7 +2,7 @@ import express from 'express'
 import * as productsControllers from '../controllers/productsControllers'
 export const router = express.Router()
 
-router.get('/',    productsControllers.getAllProducts)
-router.post('/',   productsControllers.createProduct)
+router.get('/', productsControllers.getAllProducts)
+router.post('/', productsControllers.createProduct)
 router.delete('/', productsControllers.deleteProduct)
 router.put('/', productsControllers.updateProduct)
