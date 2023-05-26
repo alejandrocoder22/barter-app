9c97e0dd429282e0532735dc88f32b344c960db6
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-05-26 23:24:28 +0200

    Removed comment

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,17 +3,17 @@ import cors from 'cors'
 import * as dotenv from 'dotenv'
 import { router as usersRouter } from './router/users'
 import { router as productsRouter } from './router/products'
+
+dotenv.config({ path: './process.env' })
+
 const app = express()
 
-dotenv.config()
-
 app.use(cors())
-
 app.use(express.json())
 
 app.use('/api/users', usersRouter)
 app.use('/api/products', productsRouter)
 
-// app.listen(process.env.PORT, () => console.log(`Listening on port ${process.env.PORT}`))
+app.listen(process.env.PORT, () => console.log(`Listening on port ${process.env.PORT}`))
 
 module.exports = app
