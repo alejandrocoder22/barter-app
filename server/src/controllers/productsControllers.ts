da5d121804a6d4814ee9d0344ee163f80c5e9d39
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-05-26 12:52:38 +0200

    Destructuring productId

--- server/src/controllers/productsControllers.ts
+++ server/src/controllers/productsControllers.ts
@@ -2,37 +2,39 @@ import * as productsServices from '../services/productsServices'
 import express from 'express'
 
 export const getAllProducts = async (_req: express.Request, res: express.Response) => {
-    try {
-        const products = await productsServices.getAllProducts()
-        res.status(200).send(products)
-    } catch (error) {
-        res.status(400).send(error)
-    }
+  try {
+    const products = await productsServices.getAllProducts()
+    res.status(200).send(products)
+  } catch (error) {
+    res.status(400).send(error)
+  }
 }
-export const createProduct = (req: express.Request, res: express.Response) => {
-    const product = req.body
-    try {
-        productsServices.createProduct(product)
-        res.status(200).send('Product created')
-    } catch (error) {
-        res.status(400).send(error)
-    }
+export const createProduct = async (req: express.Request, res: express.Response) => {
+  const product = req.body
+
+  try {
+    await productsServices.createProduct(product)
+    res.status(200).send('Product created')
+  } catch (error) {
+    console.log(error)
+    res.status(400).send(error)
+  }
 }
 export const updateProduct = (req: express.Request, res: express.Response) => {
-        const product = req.body
-    try {
-        productsServices.updateProduct(product)
-        res.status(200).send('Product updated')
-    } catch (error) {
-        res.status(400).send(error)
-    }
+  const product = req.body
+  try {
+    productsServices.updateProduct(product)
+    res.status(200).send('Product updated')
+  } catch (error) {
+    res.status(400).send(error)
+  }
 }
 export const deleteProduct = (req: express.Request, res: express.Response) => {
-    const productId = req.body
-    try {
-         productsServices.deleteProduct(productId)
-        res.status(200).send('Product deleted')
-    } catch (error) {
-          res.status(400).send(error)
-    }
+  const { productId } = req.body
+  try {
+    productsServices.deleteProduct(productId)
+    res.status(200).send('Product deleted')
+  } catch (error) {
+    res.status(400).send(error)
+  }
 }
