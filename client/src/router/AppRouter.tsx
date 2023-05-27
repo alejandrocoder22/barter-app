5ffa8328286d177b9de36a3db99b9087a96f09b8
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-05-27 13:53:49 +0200

    Added loggin and product routes

--- client/src/router/AppRouter.tsx
+++ client/src/router/AppRouter.tsx
@@ -1,5 +1,7 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from '../pages/Home';
+import Login from '../pages/Login';
+import AddProduct from '../pages/AddProduct';
 
 
 
@@ -7,6 +9,8 @@ const AppRouter = () => {
     return (
         <BrowserRouter>
             <Routes>
+                <Route path="/login" element={<Login /> }></Route>
+                <Route path="/product" element={<AddProduct /> }></Route>
                 <Route path="/" element={<Home /> }></Route>
             </Routes>
         </BrowserRouter>
