d6e006f756c38d7aec80ec5fd162ff98deddec45
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-05-27 23:49:15 +0200

    Added profile page

--- client/src/components/Nav.tsx
+++ client/src/components/Nav.tsx
@@ -8,11 +8,12 @@ const Nav = () => {
     }
   }
   return (
-    <nav className='flex justify-between max-w-screen-2xl m-auto bg-slate-400 p-2'>
-      <div className='logo'>Logo</div>
+    <nav className='flex justify-between max-w-screen-2xl m-auto  p-2'>
+      <div className='logo'><Link to='/'>Logo</Link></div>
       <ul className='flex gap-3'>
-        <li><Link to='/' className={getClassNameOnCurrentPage('/')}>Home</Link></li>
         <li><Link to='/products' className={getClassNameOnCurrentPage('/products')}>Products</Link></li>
+        <li><Link to='/product' className={getClassNameOnCurrentPage('/product')}>Add Product</Link></li>
+        <li><Link to='/profile' className={getClassNameOnCurrentPage('/profile')}>Profile</Link></li>
       </ul>
     </nav>
   )
