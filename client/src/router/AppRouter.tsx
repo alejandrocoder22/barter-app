5a7058e950521546e552b08f69678008bcb6a91e
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-05-27 23:49:39 +0200

    Added profile page

--- client/src/router/AppRouter.tsx
+++ client/src/router/AppRouter.tsx
@@ -3,18 +3,20 @@ import Home from '../pages/Home'
 import Login from '../pages/Login'
 import AddProduct from '../pages/AddProduct'
 import Products from '../pages/Products'
+import Profile from '../pages/Profile'
 import Nav from '../components/Nav'
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
-      <header>
+      <header className='bg-slate-400'>
         <Nav />
       </header>
       <Routes>
         <Route path='/login' element={<Login />} />
         <Route path='/product' element={<AddProduct />} />
         <Route path='/products' element={<Products />} />
+        <Route path='/profile' element={<Profile />} />
         <Route path='/' element={<Home />} />
       </Routes>
     </BrowserRouter>
