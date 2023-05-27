86bf1d2e1c9a3c89fa7247dd8eb9297e90c1e8ce
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-05-27 13:01:07 +0200

    Added auth middleware

---
+++ server/src/middlewares/authMiddleware.ts
@@ -0,0 +1,24 @@
+import jwt from 'jsonwebtoken'
+import express from 'express'
+
+export const protectedRoute = async (req: any, res: express.Response, next: express.NextFunction) => {
+  let token
+
+  try {
+    token = req.cookies.jwt
+
+    if (token) {
+       const secret: any =  process.env.JWT_SECRET
+      const decoded = jwt.verify(token, secret )
+
+      req.user = decoded
+
+      next()
+    } else {
+      res.status(401)
+      throw new Error('Unauthorized')
+    }
+  } catch (error: any) {
+    res.send(error.message)
+  }
+}
