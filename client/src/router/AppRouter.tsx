500730734358d22734cb18b231d5cd7730ad61a0
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-05-27 20:43:01 +0200

    Added product route

--- client/src/router/AppRouter.tsx
+++ client/src/router/AppRouter.tsx
@@ -1,20 +1,20 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Home from '../pages/Home';
-import Login from '../pages/Login';
-import AddProduct from '../pages/AddProduct';
-
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from '../pages/Home'
+import Login from '../pages/Login'
+import AddProduct from '../pages/AddProduct'
+import Products from '../pages/Products'
 
 const AppRouter = () => {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/login" element={<Login /> }></Route>
-                <Route path="/product" element={<AddProduct /> }></Route>
-                <Route path="/" element={<Home /> }></Route>
-            </Routes>
-        </BrowserRouter>
-    )
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/login' element={<Login />} />
+        <Route path='/product' element={<AddProduct />} />
+        <Route path='/products' element={<Products />} />
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default AppRouter
