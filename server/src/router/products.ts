cf2fda465c58f1b2d4d05a9ceff28c7064adf9dc
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-05-27 14:59:23 +0200

    Added multer middleware

--- server/src/router/products.ts
+++ server/src/router/products.ts
@@ -2,8 +2,9 @@ import express from 'express'
 import * as productsControllers from '../controllers/productsControllers'
 import { protectedRoute } from '../middlewares/authMiddleware'
 export const router = express.Router()
+import {upload} from '../config/multer'
 
 router.get('/', productsControllers.getAllProducts)
-router.post('/', protectedRoute,  productsControllers.createProduct)
+router.post('/', protectedRoute, upload.single('productImages'),  productsControllers.createProduct)
 router.delete('/', productsControllers.deleteProduct)
 router.put('/', productsControllers.updateProduct)
