ed80bae160eec2fd35af0f8f82eecbe496fe77a3
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-05-27 10:31:02 +0200

    Added crypto tests

---
+++ server/src/tests/router/utiils/crypto.test.js
@@ -0,0 +1,22 @@
+const { comparePasswords, encryptPassword } = require('../../../utils/crypto')
+
+describe('Test in crypto.js', () => {
+  const mockupUser = {
+    userName: 'test',
+    password: '123456'
+  }
+
+  test('EncryprPassword works correctly', async () => {
+    const hashedPassword = await encryptPassword(mockupUser)
+    expect(hashedPassword.length).toBeGreaterThan(mockupUser.password.length)
+  })
+
+  test("ComparePasswords works correctly", async () => {
+      const hashedPassword = await encryptPassword(mockupUser)
+      const resultOfComparation = await comparePasswords(mockupUser.password, hashedPassword)
+      const wrongPassword = await comparePasswords('52353252353', hashedPassword)
+      expect(resultOfComparation).toBe(true)
+      expect(wrongPassword).toBe(false)
+  })
+
+})
