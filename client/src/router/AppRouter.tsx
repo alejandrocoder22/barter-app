494972bb15bee3a0869c3ccc28590c6c63c9c415
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-05-29 21:54:47 +0200

    Added html basic structure

--- client/src/router/AppRouter.tsx
+++ client/src/router/AppRouter.tsx
@@ -5,6 +5,7 @@ import AddProduct from '../pages/AddProduct'
 import Products from '../pages/Products'
 import Profile from '../pages/Profile'
 import Nav from '../components/Nav'
+import Footer from '../components/Footer'
 
 const AppRouter = () => {
   return (
@@ -12,14 +13,17 @@ const AppRouter = () => {
       <header className='bg-slate-400'>
         <Nav />
       </header>
-      <Routes>
-        <Route path='/login' element={<Login />} />
-        <Route path='/product' element={<AddProduct />} />
-        <Route path='/products' element={<Products />} />
-        <Route path='/profile' element={<Profile />} />
-        <Route path='/' element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+      <main>
+        <Routes>
+          <Route path='/login' element={<Login />} />
+          <Route path='/product' element={<AddProduct />} />
+          <Route path='/products' element={<Products />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </main>
+      <Footer />
+main    </BrowserRouter>
   )
 }
 
