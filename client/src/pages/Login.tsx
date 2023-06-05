a8bd2005eed4a159b31f53dbc209377d2468435a
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-06-05 09:27:12 +0200

    Fixed fex styles + register link

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,6 +1,6 @@
 import { useState, useContext } from 'react'
 import { AuthContext } from '../context/authContext'
-
+import { Link } from 'react-router-dom'
 const Login = () => {
   const [userName, setUserName] = useState('')
   const [password, setPassword] = useState('')
@@ -25,13 +25,13 @@ const Login = () => {
 
   return (
     <section className='flex flex-col  items-center justify-center min-h-[calc(100vh-4rem)]'>
-      <h1 className='mb-5 text-3xl'>Login</h1>
       <form onSubmit={handleLogin} className='flex flex-col max-w-xs gap-2'>
         <label>Username</label>
         <input className='rounded-md p-1 border-2' onChange={onHandleUserName} type='text' name='username' />
         <label>Password</label>
         <input className='rounded-md p-1 border-2' onChange={onHandlePassword} type='text' name='password' />
-        <button className='bg-blue-500 hover:bg-blue-400  rounded-md p-2 mt-3' type='submit'>Login</button>
+        <p>Don't have an account?  <Link to='/register' className='text-blue-500'> Register here</Link></p>
+        <button className='text-white font-bold bg-blue-500 hover:bg-blue-400  rounded-md p-2 mt-3' type='submit'>Login</button>
 
       </form>
     </section>
--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -28,7 +28,6 @@ const Register = () => {
 
   return (
     <section className='flex flex-col  items-center justify-center min-h-[calc(100vh-4rem)]'>
-      <h1 className='mb-5 text-3xl'>Register</h1>
       <form onSubmit={handleLogin} className='flex flex-col max-w-xs gap-2'>
         <label>Email</label>
         <input className='rounded-md p-1 border-2' onChange={onHandleEmail} type='text' name='email' />
@@ -36,7 +35,7 @@ const Register = () => {
         <input className='rounded-md p-1 border-2' onChange={onHandleUserName} type='text' name='username' />
         <label>Password</label>
         <input className='rounded-md p-1 border-2' onChange={onHandlePassword} type='text' name='password' />
-        <button className='bg-blue-500 hover:bg-blue-400 mt-3 rounded-md p-2' type='submit'>Login</button>
+        <button className='text-white font-bold bg-blue-500 hover:bg-blue-400 mt-3 rounded-md p-2 color' type='submit'>Login</button>
       </form>
     </section>
   )
