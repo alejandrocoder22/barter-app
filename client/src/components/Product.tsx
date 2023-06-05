a22621bc855a952b590ce97a82e9be8b9e58a237
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-06-05 10:02:41 +0200

    Added styles to singleProduct

--- client/src/components/Product.tsx
+++ client/src/components/Product.tsx
@@ -2,8 +2,8 @@
 const Product = ({ product }) => {
   return (
     <div className='' key={product.id}>
-      <img className='w-full' src={'http://localhost:3009/' + product.imageUrl} />
-      <h2>{product.productName}</h2>
+      <img className='w-full rounded-md' src={'http://localhost:3009/' + product.imageUrl} />
+      <h2 className='capitalize'>{product.productName}</h2>
     </div>
   )
 }
