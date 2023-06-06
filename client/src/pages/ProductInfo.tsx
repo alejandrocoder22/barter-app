d5d45e8adde94c53bce35880c5ffe1051f2d63cd
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-06-06 09:50:10 +0200

    Implemented singleProduct fetching

--- client/src/pages/ProductInfo.tsx
+++ client/src/pages/ProductInfo.tsx
@@ -1,5 +1,21 @@
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+const ProductInfo = ({}) => {
+  const [product, setProduct] = useState([])
 
-const ProductInfo = () => {
+  const { productId } = useParams()
+
+  console.log(productId)
+  const getProductById = async () => {
+    const petition = await fetch(`/api/products/singleProduct/${productId}`)
+    const singleProduct = await petition.json()
+
+    setProduct(singleProduct)
+  }
+
+  useEffect(() => {
+    getProductById()
+  }, [])
   return (
     <div>ProductInfo</div>
   )
