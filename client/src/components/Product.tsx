14eb22c89f323c7e86471c9c5f9ed510977f21ad
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-06-06 09:28:21 +0200

    Added status and price to the thurnmail

--- client/src/components/Product.tsx
+++ client/src/components/Product.tsx
@@ -3,7 +3,11 @@ const Product = ({ product }) => {
   return (
     <div className=''>
       <img className='w-full rounded-md' src={'http://localhost:3009/' + product.imageUrl} />
-      <h2 className='capitalize'>{product.productName}</h2>
+      <div className='flex gap-3 items-center '>
+        <h2 className='capitalize text-xl font-bold '>{product.productName}</h2>
+        <p className='capitalize'>{product.status}</p>
+        <p className='capitalize'>{product.estimatedValue}</p>
+      </div>
     </div>
   )
 }
