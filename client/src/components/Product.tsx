ac2ae3dac69dc3c69a452973b3a0ca4aa9dc7427
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-06-06 09:50:21 +0200

    Fixed singleProduct route

--- client/src/components/Product.tsx
+++ client/src/components/Product.tsx
@@ -1,13 +1,16 @@
+import { Link } from 'react-router-dom'
 
 const Product = ({ product }) => {
   return (
     <div className=''>
-      <img className='w-full rounded-md' src={'http://localhost:3009/' + product.imageUrl} />
-      <div className='flex gap-3 items-center '>
-        <h2 className='capitalize text-xl font-bold '>{product.productName}</h2>
-        <p className='capitalize'>{product.status}</p>
-        <p className='capitalize'>{product.estimatedValue}</p>
-      </div>
+      <Link to={`/product/${product.id}`}>
+        <img className='w-full rounded-md' src={'http://localhost:3009/' + product.imageUrl} />
+        <div className='flex gap-3 items-center '>
+          <h2 className='capitalize text-xl font-bold '>{product.productName}</h2>
+          <p className='capitalize'>{product.status}</p>
+          <p className='capitalize'>{product.estimatedValue}</p>
+        </div>
+      </Link>
     </div>
   )
 }
