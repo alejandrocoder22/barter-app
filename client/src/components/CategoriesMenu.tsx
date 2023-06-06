facf68aa23f58de77fbf1d3271d137425f6bf477
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-06-06 08:52:55 +0200

    Added icons to categories

--- client/src/components/CategoriesMenu.tsx
+++ client/src/components/CategoriesMenu.tsx
@@ -1,4 +1,5 @@
 import { categories } from '../data/categories'
+
 const CategoriesMenu = ({ setCategory, category }) => {
   const onSetCategory = ({ target: { textContent } }) => {
     setCategory(textContent)
@@ -7,7 +8,12 @@ const CategoriesMenu = ({ setCategory, category }) => {
   return (
     <ul className='flex justify-center gap-2 mt-2'>
       {categories.map(categoryItem => {
-        return <li key={categoryItem} onClick={onSetCategory} className={`capitalize cursor-pointer ${category === categoryItem ? 'text-blue-500' : ''}`}>{categoryItem}</li>
+        return (
+          <div key={categoryItem.name} className=' flex  flex-col justify-center items-center bg-slate-600 rounded-full'>
+            <categoryItem.icon />
+            <li onClick={onSetCategory} className={`capitalize cursor-pointer ${category === categoryItem.name ? 'text-blue-500' : ''}`}>{categoryItem.name}</li>
+          </div>
+        )
       })}
     </ul>
   )
