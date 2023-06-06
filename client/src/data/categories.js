25874c35942c6dfe37d987f689fedbaa2fd25ba5
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-06-06 08:54:54 +0200

    Added icons to all categories

--- client/src/data/categories.js
+++ client/src/data/categories.js
@@ -1,1 +1,14 @@
-export const categories = ['all', 'crafts', 'furniture', 'electronics', 'clothes', 'car', 'other']
+
+import { GrPersonalComputer } from 'react-icons/gr'
+import { GiClothes } from 'react-icons/gi'
+import { HiOutlineDotsHorizontal } from 'react-icons/hi'
+import { AiTwotoneHome } from 'react-icons/ai'
+import { BsFillFilterSquareFill } from 'react-icons/bs'
+
+export const categories = [
+  { name: 'all', icon: BsFillFilterSquareFill },
+  { name: 'furnitures', icon: AiTwotoneHome },
+  { name: 'electronics', icon: GrPersonalComputer },
+  { name: 'clothes', icon: GiClothes },
+  { name: 'other', icon: HiOutlineDotsHorizontal }
+]
