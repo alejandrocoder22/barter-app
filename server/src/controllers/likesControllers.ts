9dcf75636584896796f30958dfbbb01d78b35bb4
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-06-07 10:02:06 +0200

    Created POST like service

---
+++ server/src/controllers/likesControllers.ts
@@ -0,0 +1,10 @@
+import * as likesServices from '../services/likesServices'
+import  { Response}  from 'express'
+export const postLike = async (req: any, res: Response) => {
+    try {
+        await likesServices.postLike(req.body.userId, req.body.productId)
+        res.status(200).send('Like Created')
+    } catch (error: any) {
+        res.send(error.message)
+    }
+}
