3e235bbbbcc6673145458a2f8804d424cc2023c6
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-06-07 10:02:34 +0200

    Added POST service

---
+++ server/src/services/likesServices.ts
@@ -0,0 +1,10 @@
+import { PrismaClient } from "@prisma/client"
+
+
+const prisma = new PrismaClient()
+export const postLike = async (userId: number, productId: number) => await prisma.like.create({
+    data: {
+        userId: userId,
+        productId: productId
+    }
+}) 
