6678414c70f3f0712a936561a74ca1719993f492
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-06-07 09:02:57 +0200

    Created the skeletoon

--- client/src/pages/ProductInfo.tsx
+++ client/src/pages/ProductInfo.tsx
@@ -17,7 +17,17 @@ const ProductInfo = ({}) => {
     getProductById()
   }, [])
   return (
-    <div>ProductInfo</div>
+    <section className='flex gap-5 items-center'>
+      <div className=''>
+        <h1 className='text-4xl font-bold mb-5 '>{product?.productName}</h1>
+        <p>{product?.status}</p>
+        <p>{product?.estimatedValue}</p>
+        <img className='max-w-3xl rounded-2xl' src={`http://localhost:3009/${product?.imageUrl}`} />
+      </div>
+      <div className=''>
+        <button className='text-white font-bold bg-lime-500 p-2 rounded-md'>Send Message</button>
+      </div>
+    </section>
   )
 }
 
