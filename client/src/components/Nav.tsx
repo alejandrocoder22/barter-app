dbd60781c51ef5c6096b21f22f7f2bfac56d6b09
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-06-09 09:13:14 +0200

    Added like page to the menu

--- client/src/components/Nav.tsx
+++ client/src/components/Nav.tsx
@@ -11,7 +11,8 @@ const Nav = () => {
     <nav className='flex justify-between max-w-screen-2xl m-auto  p-2'>
       <div className='logo'><Link to='/'>Logo</Link></div>
       <ul className='flex gap-3'>
-        <li><Link to='/products' className={getClassNameOnCurrentPage('/products')}>Products</Link></li>
+        <li><Link to='/' className={getClassNameOnCurrentPage('/')}>Products</Link></li>
+        <li><Link to='/likes' className={getClassNameOnCurrentPage('/likes')}>Likes</Link></li>
         <li><Link to='/product' className={getClassNameOnCurrentPage('/product')}>Add Product</Link></li>
         <li><Link to='/profile' className={getClassNameOnCurrentPage('/profile')}>Profile</Link></li>
       </ul>
