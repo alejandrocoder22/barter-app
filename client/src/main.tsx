967a465fa36c55df7861ddf2270f1c6170a29ba6
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-06-11 12:31:18 +0200

    Added Authcontext on main

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,10 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import AuthContextProvider from './context/authContext.tsx'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <App />
+  <AuthContextProvider>
+    <App />
+  </AuthContextProvider>
 )
