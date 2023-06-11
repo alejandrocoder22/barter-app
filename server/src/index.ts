e0ddaae4af192d542fe49eb2e4aeabd35296a520
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-06-11 08:32:05 +0200

    Added basic socked configuration

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,11 +8,11 @@ import { customErrorHandler } from './middlewares/customErrors'
 import cookiePArser from 'cookie-parser'
 import { Server } from 'socket.io'
 import { createServer } from 'http'
-
+import { getUserDataFromToken } from './utils/getUserDataFromToken'
 dotenv.config({ path: './process.env' })
 
-const httpServer = createServer()
 const app = express()
+const httpServer = createServer(app)
 
 app.use('/uploads', express.static('uploads'))
 
@@ -28,12 +28,64 @@ app.use('/api/products', productsRouter)
 const io = new Server(httpServer, {
   cors: {
     origin: 'http://localhost:5173',
-    methods: ['GET', 'POST']
+    methods: ['GET', 'POST'],
+    credentials: true
   }
 })
 
-io.on('connection', _socket => {
-  console.log('Connected')
+io.use((socket: any, next) => {
+  const token = socket.handshake.headers.cookie?.split('=')[1]
+
+  if (token) {
+    const userData: any =  getUserDataFromToken(token)
+    socket.userName = userData.userName
+    socket.userId = userData.userId
+  }
+
+    next()
+  
+
+})
+
+let users: any = []
+
+const addUserOnConnect = (userId: number, socketId: string) => {
+  !users.some((user: any) => user.userId === userId)
+  && users.push({
+    userId,
+    socketId
+
+  })
+}
+
+const getUser = (receiverId: number) => users.find((user: any) => user.userId === receiverId)
+
+const removeUserOnDisconnect = (socketId: string) => {
+  users = users.filter((user: any) => user.socketId !== socketId)
+}
+
+io.on('connection', (socket: any)  => {
+
+  console.log('User connected');
+
+ 
+    addUserOnConnect(socket.userId, socket.id)
+    io.emit('getUsers',  users)
+  // @ts-ignore
+    socket.on('sendMessage', ({recieverId, text}) => {
+    const user = getUser(recieverId)
+    io.to(user?.socketId).emit("getMessage", ({
+      userId: socket.userId,
+       text
+    }))
+    })
+
+  socket.on("disconnect", () => {
+    removeUserOnDisconnect(socket.id)
+    io.emit('getUsers',  users)
+    console.log('UIser disconnected');
+  })
+
 })
 
 httpServer.listen(process.env.PORT, () => console.log(`Listening on port ${process.env.PORT}`))
