eebe74398f1cdae8279cc49f6cc59b1bc80d98c4
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-06-11 12:31:50 +0200

    Verify user on refresh

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,25 @@
-import AuthContextProvider from './context/authContext'
+import { useEffect, useContext } from 'react'
+import { AuthContext } from './context/authContext'
 import AppRouter from './router/AppRouter'
 
 function App () {
+  const authContext = useContext(AuthContext)
+
+  const verifyUser = async () => {
+    const petition = await fetch('/api/users/verify')
+    const data = await petition.json()
+    authContext.setUser({
+      userName: data.userData.userName,
+      userId: data.userData.userId
+    })
+  }
+
+  useEffect(() => {
+    verifyUser()
+  }, [])
+
   return (
-    <AuthContextProvider>
-      <AppRouter />
-    </AuthContextProvider>
+    <AppRouter />
   )
 }
 
