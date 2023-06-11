f7ae87425b91f86f9f0c1aa48fd9f1f96b59496f
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-06-11 12:51:45 +0200

    Finised messages logic

--- client/src/components/Message.tsx
+++ client/src/components/Message.tsx
@@ -1,7 +1,14 @@
 
+import { useContext } from 'react'
+
+import { AuthContext } from '../context/authContext'
+
 const Message = ({ message }) => {
+  const authContext = useContext(AuthContext)
+
+  console.log(authContext)
   return (
-    <div>{message.text}</div>
+    <div className={`${authContext?.user?.userId === message?.senderId ? 'text-right' : 'text-left'} p-5 bg-slate-200 rounded-md m-5`}>{message.text}</div>
   )
 }
 
