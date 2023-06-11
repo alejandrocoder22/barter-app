8b4c37e2a6fe234902a7a75eec79fe6cb073174e
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-06-11 14:05:31 +0200

    Added scrollRef

--- client/src/components/Message.tsx
+++ client/src/components/Message.tsx
@@ -3,12 +3,11 @@ import { useContext } from 'react'
 
 import { AuthContext } from '../context/authContext'
 
-const Message = ({ message }) => {
+const Message = ({ message, scrollRef }) => {
   const authContext = useContext(AuthContext)
 
-  console.log(authContext)
   return (
-    <div className={`${authContext?.user?.userId === message?.senderId ? 'text-right' : 'text-left'} p-5 bg-slate-200 rounded-md m-5`}>{message.text}</div>
+    <div className={`${authContext?.user?.userId === message?.senderId ? 'text-right' : 'text-left'} p-5 bg-slate-200 rounded-md m-5`} ref={scrollRef}>{message.text}</div>
   )
 }
 
