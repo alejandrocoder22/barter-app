e347edba3f73143aeaeff59d75db6d68122ad0fd
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-06-11 14:40:00 +0200

    Fixed bug fetching conversations

--- client/src/components/Conversations.tsx
+++ client/src/components/Conversations.tsx
@@ -1,10 +1,13 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useContext } from 'react'
+import { AuthContext } from '../context/authContext'
 
 const Conversations = ({ conver, setConversationId }) => {
+  const authContext = useContext(AuthContext)
   const [user, setUSer] = useState({})
 
   const getUserData = async () => {
-    const petition = await fetch(`/api/users/${conver.receiverId}`)
+    const remainingId = conver.receiverId === authContext?.user?.userId ? conver.senderId : conver.receiverId
+    const petition = await fetch(`/api/users/${remainingId}`)
     const data = await petition.json()
     setUSer(data)
   }
