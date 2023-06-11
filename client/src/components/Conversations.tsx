64eafc3106b5cfd4650e83fb7ce07a98f8ac6199
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-06-11 12:31:28 +0200

    Fetch users

--- client/src/components/Conversations.tsx
+++ client/src/components/Conversations.tsx
@@ -1,8 +1,20 @@
+import { useEffect, useState } from 'react'
 
-const Conversations = ({ conversations }) => {
+const Conversations = ({ conver, setConversationId }) => {
+  const [user, setUSer] = useState({})
+
+  const getUserData = async () => {
+    const petition = await fetch(`/api/users/${conver.receiverId}`)
+    const data = await petition.json()
+    setUSer(data)
+  }
+
+  useEffect(() => {
+    getUserData()
+  }, [])
   return (
-    <div className='flex items-center justify-center gap-3 cursor-pointer'>
-      <span className='font-bold'>Alvaro</span>
+    <div onClick={() => setConversationId(conver.id)} className='flex items-center justify-center gap-3 cursor-pointer'>
+      <span className='font-bold'>{user.userName}</span>
       <img className='h-9 rounded-full' src='https://picsum.photos/200' alt='' />
     </div>
   )
