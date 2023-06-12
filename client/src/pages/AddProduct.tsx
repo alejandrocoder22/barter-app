9ea1eb9039e8599e97e3fa54023de019332b13ce
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-06-12 09:19:44 +0200

    Fixed Add product logic

--- client/src/pages/AddProduct.tsx
+++ client/src/pages/AddProduct.tsx
@@ -2,23 +2,29 @@ import { useState } from 'react'
 
 const AddProduct = () => {
   const [productName, setProductName] = useState('')
-  const [estimatedValue, setEstimatedValue] = useState(0)
+  const [estimatedValue, setEstimatedValue] = useState('')
   const [status, setStatus] = useState('')
-  const [imageUrl, setImageUrl] = useState('')
   const [category, setCategory] = useState('')
+  const [location, setLocation] = useState('')
+  const [description, setDescription] = useState('')
+  const [file, setFile] = useState(null)
 
-  const handleLogin = (e) => {
+  const handleLogin = async (e) => {
     e.preventDefault()
-    fetch('/api/products', {
+    const formData = new FormData()
+
+    formData.append('productName', productName)
+    formData.append('estimatedValue', estimatedValue)
+    formData.append('status', status)
+    formData.append('category', category)
+    formData.append('location', location)
+    formData.append('description', description)
+    formData.append('productImages', file)
+
+    console.log(formData)
+    await fetch('/api/products', {
       method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        productName,
-        estimatedValue,
-        status,
-        imageUrl,
-        category
-      })
+      body: formData
     })
   }
 
@@ -26,7 +32,6 @@ const AddProduct = () => {
     <main className='min-h-screen '>
       <div className='flex flex-col min-h-screen justify-center items-center item bg-cyan-50'>
         <form onSubmit={handleLogin} className='flex flex-col max-w-xs gap-1'>
-
           <label>Product Name</label>
           <input onChange={(e) => setProductName(e.target.value)} className='border-2' type='text' />
           <label>Estimated Value</label>
@@ -34,9 +39,13 @@ const AddProduct = () => {
           <label>State</label>
           <input onChange={(e) => setStatus(e.target.value)} className='border-2' type='text' />
           <label>Image</label>
-          <input onChange={(e) => setImageUrl(e.target.value)} className='border-2' type='text' />
+          <input onChange={(e) => setFile(e.target.files[0])} className='border-2' type='file' accept='image/*' />
           <label>Category</label>
           <input onChange={(e) => setCategory(e.target.value)} className='border-2' type='text' />
+          <label>Location</label>
+          <input onChange={(e) => setLocation(e.target.value)} className='border-2' type='text' />
+          <label>Description</label>
+          <textarea onChange={(e) => setDescription(e.target.value)} className='border-2' />
           <button className='bg-indigo-500 mt-2 p-2 hover:bg-indigo-400' type='submit'>Login</button>
         </form>
       </div>
