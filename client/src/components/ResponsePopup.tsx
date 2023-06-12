c0176649cd3e327a68e313fbe3555797adc80693
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-06-12 22:46:27 +0200

    Added popup styles

--- client/src/components/ResponsePopup.tsx
+++ client/src/components/ResponsePopup.tsx
@@ -1,6 +1,8 @@
 const ResponsePopup = ({ message, status }) => {
   return (
-    <div className={`fixed right-5  top-12 bg-slate-800 p-5 rounded-md text-white font-bold ${status.ok ? 'bg-green-400' : 'bg-red-400'}`}>{message}</div>
+    <div className={`after:content-[''] after:absolute after:bottom-0 after:right-0 after:h-3 after:w-full fixed right-5 top-12  p-5 rounded-md text-white font-bold ${status.ok ? 'bg-green-400' : 'bg-red-400'}`}>
+      {message}
+    </div>
   )
 }
 
