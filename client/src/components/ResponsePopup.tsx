49de4a89e890a28b117cb3039448a5370afc6ac8
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-06-12 23:01:13 +0200

    Added animation to popup

--- client/src/components/ResponsePopup.tsx
+++ client/src/components/ResponsePopup.tsx
@@ -1,6 +1,7 @@
 const ResponsePopup = ({ message, status }) => {
+  console.log(message)
   return (
-    <div className={`after:content-[''] after:absolute after:bottom-0 after:right-0 after:h-3 after:w-full fixed right-5 top-12  p-5 rounded-md text-white font-bold ${status.ok ? 'bg-green-400' : 'bg-red-400'}`}>
+    <div className={`after:content-[''] after:absolute after:bottom-0 after:right-0 after:h-3 after:w-full fixed right-5 top-12  p-5 rounded-md text-white font-bold transition-transform duration-700  ${status.ok ? 'bg-green-400' : 'bg-red-400'} ${message.length > 0 ? 'transform translate-x-0' : 'transform translate-x-72'}`}>
       {message}
     </div>
   )
