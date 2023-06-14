2c21f71e395b1e4eef784bbed4c27398989d21fe
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-06-14 08:51:39 +0200

    Refactor to add customHook

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -3,23 +3,19 @@ import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import ResponsePopup from '../components/ResponsePopup'
 import useAuth from '../hooks/useAuth'
+import useForm from '../hooks/useForm'
 const Login = () => {
-  const [userName, setUserName] = useState('')
-  const [password, setPassword] = useState('')
-
-  const { handleLogin, responseMessage, responseStatus } = useAuth({ userName, password })
-
-  const onHandleUserName = (e: any) => setUserName(e.target.value)
-  const onHandlePassword = (e: any) => setPassword(e.target.value)
+  const { handleForm, form } = useForm()
+  const { handleLogin, responseMessage, responseStatus } = useAuth({ userName: form.userName, password: form.password })
 
   return (
     <section className='flex flex-col  items-center justify-center min-h-[calc(100vh-4rem)]'>
       <ResponsePopup message={responseMessage} status={responseStatus} />
-      <form onSubmit={async (e) => await handleLogin(e, userName, password)} className='flex flex-col max-w-xs gap-2'>
+      <form onSubmit={async (e) => await handleLogin(e, form)} className='flex flex-col max-w-xs gap-2'>
         <label>Username</label>
-        <input className='rounded-md p-1 border-2' onChange={onHandleUserName} type='text' name='username' />
+        <input className='rounded-md p-1 border-2' onChange={handleForm} type='text' name='userName' />
         <label>Password</label>
-        <input className='rounded-md p-1 border-2' onChange={onHandlePassword} type='password' name='password' />
+        <input className='rounded-md p-1 border-2' onChange={handleForm} type='password' name='password' />
         <p>Don't have an account?  <Link to='/register' className='text-blue-500'> Register here</Link></p>
         <button className='text-white font-bold bg-blue-500 hover:bg-blue-400  rounded-md p-2 mt-3' type='submit'>Login</button>
       </form>
