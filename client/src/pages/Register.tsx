5312b00184f64f39440e1b69e2a99f7710140755
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-06-14 09:08:09 +0200

    Added errorpopUp

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,12 +1,14 @@
 import useForm from '../hooks/useForm'
 import useAuth from '../hooks/useAuth'
+import ResponsePopup from '../components/ResponsePopup'
 
 const Register = () => {
   const { handleForm, form } = useForm()
-  const { handleRegister } = useAuth({ userName: form.userName, password: form.password, email: form.email })
+  const { handleRegister, responseMessage, responseStatus } = useAuth({ userName: form.userName, password: form.password, email: form.email })
 
   return (
     <section className='flex flex-col  items-center justify-center min-h-[calc(100vh-4rem)]'>
+      <ResponsePopup message={responseMessage} status={responseStatus} />
       <form onSubmit={handleRegister} className='flex flex-col max-w-xs gap-2'>
         <label>Email</label>
         <input className='rounded-md p-1 border-2' onChange={handleForm} type='text' name='email' />
