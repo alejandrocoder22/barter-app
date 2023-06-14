02b9a08d4987a84174e2c185ac3aa95091dff3d4
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-06-14 09:17:00 +0200

    Added ? operator to prevent crash in render

--- client/src/pages/LikedProducts.tsx
+++ client/src/pages/LikedProducts.tsx
@@ -16,7 +16,7 @@ const LikedProducts = () => {
   return (
     <>
       <section className='grid grid-cols-3 gap-5 max-w-screen-2xl m-auto  p-2'>
-        {likedProducts.map(product => (
+        {likedProducts?.map(product => (
           <Product key={product.id} product={product.product} />
         ))}
       </section>
