62d11c803863c17efc8552219dbce366b2cefd16
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-06-14 08:58:33 +0200

    Refactor register to match new useForm hook and handleRegister

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,40 +1,19 @@
-import { useState, useContext } from 'react'
-import { AuthContext } from '../context/authContext'
+import useForm from '../hooks/useForm'
+import useAuth from '../hooks/useAuth'
 
 const Register = () => {
-  const [userName, setUserName] = useState('')
-  const [password, setPassword] = useState('')
-  const [email, setEmail] = useState('')
-
-  const { setUser } = useContext(AuthContext)
-  const handleLogin = (e) => {
-    e.preventDefault()
-    fetch('/api/users/register', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        userName,
-        password,
-        email
-      })
-    }).then(async res => await res.json()).then(data => {
-      setUser({ userName: data.userName })
-    })
-  }
-
-  const onHandleUserName = (e: any) => setUserName(e.target.value)
-  const onHandlePassword = (e: any) => setPassword(e.target.value)
-  const onHandleEmail = (e: any) => setEmail(e.target.value)
+  const { handleForm, form } = useForm()
+  const { handleRegister } = useAuth({ userName: form.userName, password: form.password, email: form.email })
 
   return (
     <section className='flex flex-col  items-center justify-center min-h-[calc(100vh-4rem)]'>
-      <form onSubmit={handleLogin} className='flex flex-col max-w-xs gap-2'>
+      <form onSubmit={handleRegister} className='flex flex-col max-w-xs gap-2'>
         <label>Email</label>
-        <input className='rounded-md p-1 border-2' onChange={onHandleEmail} type='text' name='email' />
+        <input className='rounded-md p-1 border-2' onChange={handleForm} type='text' name='email' />
         <label>Username</label>
-        <input className='rounded-md p-1 border-2' onChange={onHandleUserName} type='text' name='username' />
+        <input className='rounded-md p-1 border-2' onChange={handleForm} type='text' name='userName' />
         <label>Password</label>
-        <input className='rounded-md p-1 border-2' onChange={onHandlePassword} type='password' name='password' />
+        <input className='rounded-md p-1 border-2' onChange={handleForm} type='password' name='password' />
         <button className='text-white font-bold bg-blue-500 hover:bg-blue-400 mt-3 rounded-md p-2 color' type='submit'>Login</button>
       </form>
     </section>
