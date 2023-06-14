8e5a1546d2b5237a6d88eb50ec200abaadaa6756
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-06-14 09:35:17 +0200

    Refactor handleProduct

--- client/src/pages/AddProduct.tsx
+++ client/src/pages/AddProduct.tsx
@@ -1,31 +1,26 @@
 import { useState } from 'react'
+import useForm from '../hooks/useForm'
 
 const AddProduct = () => {
-  const [productName, setProductName] = useState('')
-  const [estimatedValue, setEstimatedValue] = useState('')
-  const [status, setStatus] = useState('')
-  const [category, setCategory] = useState('')
-  const [location, setLocation] = useState('')
-  const [description, setDescription] = useState('')
   const [file, setFile] = useState(null)
 
+  const { handleForm, form } = useForm()
   const handleLogin = async (e) => {
     e.preventDefault()
+
     const formData = new FormData()
 
-    formData.append('productName', productName)
-    formData.append('estimatedValue', estimatedValue)
-    formData.append('status', status)
-    formData.append('category', category)
-    formData.append('location', location)
-    formData.append('description', description)
+    Object.keys(form).forEach((key) => {
+      formData.append(key, form[key])
+    })
     formData.append('productImages', file)
 
-    console.log(formData)
-    await fetch('/api/products', {
+    const petition = await fetch('/api/products', {
       method: 'POST',
       body: formData
     })
+
+    const response = await petition.json()
   }
 
   return (
@@ -33,19 +28,31 @@ const AddProduct = () => {
       <div className='flex flex-col min-h-screen justify-center items-center item bg-cyan-50'>
         <form onSubmit={handleLogin} className='flex flex-col max-w-xs gap-1'>
           <label>Product Name</label>
-          <input onChange={(e) => setProductName(e.target.value)} className='border-2' type='text' />
+          <input onChange={handleForm} className='border-2' type='text' name='productName' />
           <label>Estimated Value</label>
-          <input onChange={(e) => setEstimatedValue(e.target.value)} className='border-2' type='text' />
+          <input onChange={handleForm} className='border-2' type='number' name='estimatedValue' />
           <label>State</label>
-          <input onChange={(e) => setStatus(e.target.value)} className='border-2' type='text' />
+          <select onChange={handleForm} name='status'>
+            <option defaultChecked>Choose a state</option>
+            <option value='secondhand'>Segunda mano</option>
+            <option value='new'>Nuevo</option>
+          </select>
           <label>Image</label>
           <input onChange={(e) => setFile(e.target.files[0])} className='border-2' type='file' accept='image/*' />
           <label>Category</label>
-          <input onChange={(e) => setCategory(e.target.value)} className='border-2' type='text' />
+          <select onChange={handleForm} name='category'>
+            <option defaultChecked>Choose an category</option>
+            <option value='car'>Nuevo</option>
+            <option value='other'>Segunda mano</option>
+            <option value='furnitures'>Nuevo</option>
+            <option value='electronics'>Nuevo</option>
+            <option value='clothes'>Nuevo</option>
+            <option value='crafts'>Nuevo</option>
+          </select>
           <label>Location</label>
-          <input onChange={(e) => setLocation(e.target.value)} className='border-2' type='text' />
+          <input onChange={handleForm} className='border-2' type='text' name='location' />
           <label>Description</label>
-          <textarea onChange={(e) => setDescription(e.target.value)} className='border-2' />
+          <textarea onChange={handleForm} className='border-2' name='description' />
           <button className='bg-indigo-500 mt-2 p-2 hover:bg-indigo-400' type='submit'>Login</button>
         </form>
       </div>
