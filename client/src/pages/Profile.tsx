fead09368dcb22bd1a47d0ec3dada9b01caadf76
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-06-26 08:42:11 +0200

    Use icon to upload image and hidden default input

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -35,8 +35,10 @@ const Profile = () => {
     <section className='flex min-h-[calc(100vh-5rem)]'>
       <div className='relative bg-slate-500 h-80 w-80 rounded-full'>
         <img src={`http://localhost:3009/${user?.profileImg}`} className='w-full h-full object-cover rounded-full ' />
-        <RiUploadCloudFill className='absolute bottom-5 right-12 block h-10 w-10  text-gray-200' />
-        <input onChange={uploadImage} type='file' />
+        <input className='hidden' onChange={uploadImage} type='file' id='file' name='file' />
+        <label htmlFor='file' id='file'>
+          <RiUploadCloudFill className='absolute bottom-5 right-12 block h-10 w-10 cursor-pointer text-gray-200' />
+        </label>
       </div>
     </section>
   )
