c65208dd3a933adaa97d1db7f9314738a5d8ba1f
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-07-06 18:25:02 +0200

    Added types file

---
+++ client/src/types.d.ts
@@ -0,0 +1,14 @@
+export interface Product {
+  id: number
+  productName: string
+  user: string
+  userId: number
+  estimatedValue: number
+  status: string
+  description: string
+  location: string
+  imageUrl: string
+  category: string
+  createdAy: string
+  updatedAt: string
+}
