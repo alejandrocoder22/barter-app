9736213820929c6499719654300051284cb1f21c
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-07-11 09:23:35 +0200

    Added delete and update icons

--- client/src/components/ProductByUser.tsx
+++ client/src/components/ProductByUser.tsx
@@ -1,10 +1,16 @@
 
+import { MdDelete, MdModeEditOutline } from 'react-icons/md'
+
 const ProductByUser = ({ products }) => {
   return (
     products.map(product =>
       <div key={product.id} className=''>
-        <img src={`http://localhost:3009/${product.imageUrl}`} alt='' />
+        <img className='w-1/4' src={`http://localhost:3009/${product.imageUrl}`} alt='' />
         <div className=''>{product.description}</div>
+        <div className='flex gap-3'>
+          <MdDelete className='text-3xl cursor-pointer' />
+          <MdModeEditOutline className='text-3xl cursor-pointer' />
+        </div>
       </div>
     )
 
