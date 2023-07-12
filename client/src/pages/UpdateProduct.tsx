4ca11c3ad55b7b294e0e49cb9281517957ae9f10
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-07-12 09:21:14 +0200

    Created updateProduct skeletoon

--- client/src/pages/UpdateProduct.tsx
+++ client/src/pages/UpdateProduct.tsx
@@ -1,6 +1,44 @@
+import useForm from '../hooks/useForm'
+import { useState, useEffect } from 'react'
+import { getProductById } from '../services/products'
+import { useParams } from 'react-router-dom'
 const UpdatedProduct: React.FC = () => {
+  const { form, handleForm } = useForm()
+  const [file, setFile] = useState()
+  const [product, setProduct] = useState()
+
+  const { productId } = useParams()
+
+  useEffect(() => {
+    getProductById(Number(productId), setProduct)
+  }, [productId])
+
+  console.log(product)
+
   return (
-    <h1>Updated Product Page</h1>
+    <main className='min-h-screen '>
+      <div className='flex flex-col min-h-screen justify-center items-center item '>
+        <form className='flex flex-col max-w-xs gap-1'>
+          <label>Product Name</label>
+          <input value={product.productName} onChange={handleForm} className='border-2' type='text' name='productName' />
+          <label>Estimated Value</label>
+          <input onChange={handleForm} className='border-2' type='number' name='estimatedValue' />
+          <label>State</label>
+          <select className='border-2' onChange={handleForm} name='status'>
+            <option defaultChecked>Choose a state</option>
+            <option value='secondhand'>Segunda mano</option>
+            <option value='new'>Nuevo</option>
+          </select>
+          <label>Image</label>
+          <input onChange={(e) => setFile(e.target.files[0])} className='border-2' type='file' accept='image/*' />
+          <label>Location</label>
+          <input onChange={handleForm} className='border-2' type='text' name='location' />
+          <label>Description</label>
+          <textarea onChange={handleForm} className='border-2' name='description' />
+          <button className='bg-indigo-500 mt-2 p-2 hover:bg-indigo-400' type='submit'>Update product</button>
+        </form>
+      </div>
+    </main>
   )
 }
 
