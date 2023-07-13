b770978e4e6bf87d598102185a7e69ceff68453b
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-07-13 08:56:06 +0200

    Fill updateInfo with current data

--- client/src/pages/UpdateProduct.tsx
+++ client/src/pages/UpdateProduct.tsx
@@ -5,24 +5,46 @@ import { useParams } from 'react-router-dom'
 const UpdatedProduct: React.FC = () => {
   const { form, handleForm } = useForm()
   const [file, setFile] = useState()
-  const [product, setProduct] = useState()
+  const [product, setProduct] = useState({})
+  const [tempProduct, setTempProduct] = useState()
 
   const { productId } = useParams()
 
   useEffect(() => {
-    getProductById(Number(productId), setProduct)
+    getProductById(Number(productId), setProduct, setProduct)
+    setTempProduct({ ...product })
   }, [productId])
-
-  console.log(product)
 
   return (
     <main className='min-h-screen '>
       <div className='flex flex-col min-h-screen justify-center items-center item '>
         <form className='flex flex-col max-w-xs gap-1'>
           <label>Product Name</label>
-          <input value={product.productName} onChange={handleForm} className='border-2' type='text' name='productName' />
+          <input
+            value={tempProduct?.productName}
+            onChange={(e) => setTempProduct(
+              {
+                ...tempProduct,
+                [e.target.name]: e.target.value
+              }
+            )}
+            className='border-2'
+            type='text'
+            name='productName'
+          />
           <label>Estimated Value</label>
-          <input onChange={handleForm} className='border-2' type='number' name='estimatedValue' />
+          <input
+            value={tempProduct?.estimatedValue}
+            onChange={(e) => setTempProduct(
+              {
+                ...tempProduct,
+                [e.target.name]: e.target.value
+              }
+            )}
+            className='border-2'
+            type='number'
+            name='estimatedValue'
+          />
           <label>State</label>
           <select className='border-2' onChange={handleForm} name='status'>
             <option defaultChecked>Choose a state</option>
@@ -32,9 +54,31 @@ const UpdatedProduct: React.FC = () => {
           <label>Image</label>
           <input onChange={(e) => setFile(e.target.files[0])} className='border-2' type='file' accept='image/*' />
           <label>Location</label>
-          <input onChange={handleForm} className='border-2' type='text' name='location' />
+          <input
+            value={tempProduct?.location}
+            onChange={(e) => setTempProduct(
+              {
+                ...tempProduct,
+                [e.target.name]: e.target.value
+              }
+            )}
+            className='border-2'
+            type='text'
+            name='location'
+          />
           <label>Description</label>
-          <textarea onChange={handleForm} className='border-2' name='description' />
+          <textarea
+            value={tempProduct?.description}
+            onChange={(e) => setTempProduct(
+              {
+                ...tempProduct,
+                [e.target.name]: e.target.value
+              }
+            )}
+            className='border-2'
+            type='text'
+            name='description'
+          />
           <button className='bg-indigo-500 mt-2 p-2 hover:bg-indigo-400' type='submit'>Update product</button>
         </form>
       </div>
