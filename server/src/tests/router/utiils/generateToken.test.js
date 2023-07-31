de8f196b3d915a617c2acab9d01f9e2aaafa44f7
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-07-31 09:42:49 +0200

    Added test to generateToken

---
+++ server/src/tests/router/utiils/generateToken.test.js
@@ -0,0 +1,18 @@
+const generateToken = require('../../../utils/generateToken')
+const express = require('express')
+
+
+
+const mockupUser = {
+    userId: '2',
+    userName: 'test',
+    password: '123456' 
+}
+
+describe('Tests in generateToken', () => {
+
+    it('Should generate token correctly', ()=> {
+      const response =  generateToken(express.response, mockupUser, process.env.JWT_SECRET)
+      console.log(response)
+    })
+})
