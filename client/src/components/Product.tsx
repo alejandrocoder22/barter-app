33604b3e19f3b773a53aa8221363588eebd6a39a
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-08-09 12:28:33 +0200

    Aling info title to center

--- client/src/components/Product.tsx
+++ client/src/components/Product.tsx
@@ -6,9 +6,9 @@ const Product = ({ product }) => {
     <div className='mt-6'>
       <Link to={`/product/${product.id}`}>
         <img className=' h-[267px] w-full rounded-[50px] object-cover' src={'http://localhost:3009/' + product.imageUrl} />
-        <div className='flex gap-3 items-center justify-between '>
-          <h2 className='capitalize font-bold  mt-2 text-[1rem]'>{product.productName}</h2>
-          <div className='flex gap-3'>
+        <div className='flex gap-3 items-center justify-between mt-2  '>
+          <h2 className='capitalize font-bold  text-[1rem]'>{product.productName}</h2>
+          <div className='flex gap-3 items-center'>
             <p className='capitalize text-[0.875rem]'>{product.estimatedValue}$</p>
             <BlackHearthIcon />
           </div>
