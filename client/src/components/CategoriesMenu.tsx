eefb7f6e37e82b8d1935595a3cf884306a0ec0de
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-08-09 11:37:11 +0200

    Addwed styles to icons

--- client/src/components/CategoriesMenu.tsx
+++ client/src/components/CategoriesMenu.tsx
@@ -5,9 +5,9 @@ const CategoriesMenu = ({ setCategory, singleCategory }) => {
   }
   return (
 
-    <div onClick={onSetCategory} className={`p-3 cursor-pointer flex flex-col justify-center items-center  rounded-full mt-5 mb-3 ${singleCategory === singleCategory.name ? 'bg-gray-200' : ''}`}>
-      <singleCategory.icon className='w-5 h-5 cursor-pointer block pointer-events-none' />
-      <li className='capitalize cursor-pointer pointer-events-none  text-gray-500'>{singleCategory.name}</li>
+    <div onClick={onSetCategory} className={`p-3 cursor-pointer flex flex-col justify-center items-center  rounded-full mt-5 mb- ${singleCategory === singleCategory.name ? 'bg-gray-200' : ''}`}>
+      <singleCategory.icon iconStyle='cursor-pointer block pointer-events-none' />
+      <li className='capitalize cursor-pointer pointer-events-none mt-2 text-gray-500'>{singleCategory.name}</li>
     </div>
 
   )
