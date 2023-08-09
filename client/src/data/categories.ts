170a8a4f8f4748110e0b9b5400fdebd320849bb8
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-08-09 11:13:07 +0200

    Added all icons from SVG

--- client/src/data/categories.ts
+++ client/src/data/categories.ts
@@ -1,16 +1,12 @@
 
-import { GrPersonalComputer } from 'react-icons/gr'
-import { GiClothes, GiStoneCrafting } from 'react-icons/gi'
-import { HiOutlineDotsHorizontal } from 'react-icons/hi'
-import { AiTwotoneHome, AiFillCar } from 'react-icons/ai'
-import { BsFillFilterSquareFill } from 'react-icons/bs'
+import { AllProductsIcon, CraftIcon, FurnitureIcon, CarIcon, ClothesIcon, ElectronicIcon, OtherIcon } from '../components/Icons'
 
 export const categories: Array<{ name: string, icon: SVGAElement }> = [
-  { name: 'all', icon: BsFillFilterSquareFill },
-  { name: 'furnitures', icon: AiTwotoneHome },
-  { name: 'electronics', icon: GrPersonalComputer },
-  { name: 'clothes', icon: GiClothes },
-  { name: 'crafts', icon: GiStoneCrafting },
-  { name: 'car', icon: AiFillCar },
-  { name: 'other', icon: HiOutlineDotsHorizontal }
+  { name: 'all', icon: AllProductsIcon },
+  { name: 'furnitures', icon: FurnitureIcon },
+  { name: 'electronics', icon: ElectronicIcon },
+  { name: 'clothes', icon: ClothesIcon },
+  { name: 'crafts', icon: CraftIcon },
+  { name: 'car', icon: CarIcon },
+  { name: 'other', icon: OtherIcon }
 ]
