7366a723db12cfa3728a99874f3848d9a6a43f39
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-08-10 19:01:58 +0200

    Added margin to product Name

--- client/src/components/Product.tsx
+++ client/src/components/Product.tsx
@@ -7,7 +7,7 @@ const Product = ({ product }) => {
       <Link to={`/product/${product.id}`}>
         <img className=' h-[267px] w-full rounded-[50px] object-cover' src={'http://localhost:3009/' + product.imageUrl} />
         <div className='flex gap-3 items-center justify-between mt-2  '>
-          <h2 className='capitalize font-bold  text-[1rem]'>{product.productName}</h2>
+          <h2 className='capitalize font-bold ml-2  text-[1rem]'>{product.productName}</h2>
           <div className='flex gap-3 items-center'>
             <p className='capitalize text-[0.875rem]'>{product.estimatedValue}$</p>
             <BlackHearthIcon />
