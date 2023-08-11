33819ae6e7a3ba4a669e0e8749b1ed766802dd75
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-08-11 11:21:52 +0200

    Reduced margin and border radius

--- client/src/components/Product.tsx
+++ client/src/components/Product.tsx
@@ -5,9 +5,9 @@ const Product = ({ product }) => {
   return (
     <div className='mt-6'>
       <Link to={`/product/${product.id}`}>
-        <img className=' h-[267px] w-full rounded-[50px] object-cover' src={'http://localhost:3009/' + product.imageUrl} />
+        <img className=' h-[267px] w-full rounded-[15px] object-cover' src={'http://localhost:3009/' + product.imageUrl} />
         <div className='flex gap-3 items-center justify-between mt-2  '>
-          <h2 className='capitalize font-bold ml-2  text-[1rem]'>{product.productName}</h2>
+          <h2 className='capitalize font-bold  text-[1rem]'>{product.productName}</h2>
           <div className='flex gap-3 items-center'>
             <p className='capitalize text-[0.875rem]'>{product.estimatedValue}$</p>
             <BlackHearthIcon />
