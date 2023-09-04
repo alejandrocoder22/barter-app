674b8411e91720878432f033cf1b3fbbc04c7f9b
alejandrocoder22 <alejandro2210lp@gmail.com>
2023-09-04 09:23:05 +0200

    Removed duplicated main

--- client/src/pages/AddProduct.tsx
+++ client/src/pages/AddProduct.tsx
@@ -9,31 +9,29 @@ const AddProduct = () => {
   const { handleForm, form } = useForm()
 
   return (
-    <main className='min-h-screen '>
-      <div className='flex flex-col min-h-screen justify-center items-center item '>
-        <form onSubmit={async (e) => await addProduct(e, form, file)} className='flex flex-col max-w-xs gap-1'>
-          <label>Product Name</label>
-          <input onChange={handleForm} className='border-2' type='text' name='productName' />
-          <label>Estimated Value</label>
-          <input onChange={handleForm} className='border-2' type='number' name='estimatedValue' />
-          <label>State</label>
-          <select className='border-2' onChange={handleForm} name='status'>
-            <option defaultChecked>Choose a state</option>
-            <option value='secondhand'>Segunda mano</option>
-            <option value='new'>Nuevo</option>
-          </select>
-          <label>Image</label>
-          <input onChange={(e) => setFile(e.target.files[0])} className='border-2' type='file' accept='image/*' />
-          <label>Category</label>
-          <SelectCategory handleForm={handleForm} />
-          <label>Location</label>
-          <input onChange={handleForm} className='border-2' type='text' name='location' />
-          <label>Description</label>
-          <textarea onChange={handleForm} className='border-2' name='description' />
-          <button className='bg-indigo-500 mt-2 p-2 hover:bg-indigo-400' type='submit'>Add product</button>
-        </form>
-      </div>
-    </main>
+    <div className='flex flex-col min-h-screen justify-center items-center item '>
+      <form onSubmit={async (e) => await addProduct(e, form, file)} className='flex flex-col max-w-xs gap-1'>
+        <label>Product Name</label>
+        <input onChange={handleForm} className='border-2' type='text' name='productName' />
+        <label>Estimated Value</label>
+        <input onChange={handleForm} className='border-2' type='number' name='estimatedValue' />
+        <label>State</label>
+        <select className='border-2' onChange={handleForm} name='status'>
+          <option defaultChecked>Choose a state</option>
+          <option value='secondhand'>Segunda mano</option>
+          <option value='new'>Nuevo</option>
+        </select>
+        <label>Image</label>
+        <input onChange={(e) => setFile(e.target.files[0])} className='border-2' type='file' accept='image/*' />
+        <label>Category</label>
+        <SelectCategory handleForm={handleForm} />
+        <label>Location</label>
+        <input onChange={handleForm} className='border-2' type='text' name='location' />
+        <label>Description</label>
+        <textarea onChange={handleForm} className='border-2' name='description' />
+        <button className='bg-indigo-500 mt-2 p-2 hover:bg-indigo-400' type='submit'>Add product</button>
+      </form>
+    </div>
   )
 }
 
